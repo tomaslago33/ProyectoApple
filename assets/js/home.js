c02326274600822ef70d7710b3a009ada5d3ab7c
Homero Benjamín Celano <benjaminncelano@gmail.com>
2025-06-06 20:54:40 -0300

    Init carousel

---
+++ assets/js/home.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    });
+});
