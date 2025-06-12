a58fa204885f1fd5a10ba4f6d0c1746f596163ed
Homero Benjamín Celano <benjaminncelano@gmail.com>
2025-06-11 22:09:14 -0300

    carousel incomplete

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper('.swiper', {
-        slidesPerView: 1,
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 6,
         loop: true,
         pagination: {
-            el: '.swiper-pagination',
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            el: ".swiper-pagination",
+            clickable: true,
         }
     });
 });
