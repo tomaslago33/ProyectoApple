f9be0dfd7d283c9eeedaaecc8e1782f603f6cdbf
Homero Benjamín Celano <benjaminncelano@gmail.com>
2025-07-14 14:34:13 -0300

    Continuando NavBar

--- assets/js/home.js
+++ assets/js/home.js
@@ -11,6 +11,24 @@ document.addEventListener('DOMContentLoaded', () => {
         autoplay: {
             delay: 3000,
         },
-        speed: 800,
+        speed: 800
+    });
+
+    document.querySelectorAll('.navItem').forEach(element => {
+        element.addEventListener('mouseenter', () => {
+            document.querySelector('#subNav').style.height = '350px';
+            document.querySelector('#subNav').style.paddingTop = '1rem';
+            document.querySelector('main').style.filter = 'blur(3px)';
+            document.querySelector('footer').style.filter = 'blur(3px)';
+        });
+    });
+
+    document.querySelector('#subNav').addEventListener('mouseleave', () => {
+        if (!document.querySelector('nav').matches(':hover')) {
+            document.querySelector('#subNav').style.height = '0';
+            document.querySelector('#subNav').style.paddingTop = '0';
+            document.querySelector('main').style.filter = 'none';
+            document.querySelector('footer').style.filter = 'none';
+        };
     });
 });
